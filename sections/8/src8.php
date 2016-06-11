93c3d0947fa4f26f1bee8e82be8f74282aa8f98e
Oleg Pedorenko <olezhkoo@gmail.com>
2016-06-11 15:57:29 +0300

    Added Section 8

---
+++ sections/8/src8.php
@@ -0,0 +1,53 @@
+<?php
+/* 
+	$a = 1 + 1;
+	echo "1 + 1 is $a\n";
+	$typea = gettype($a);
+	echo "Type: $typea \n";
+ */
+  
+  
+/* 	$b = "1" + "1";
+	echo "\"1\" + \"1\" is $b \n";
+	$typeb = gettype($b);
+	echo "Type: $typeb \n";
+
+   */
+
+/* $c = "1" + 1;
+echo "\"1\" + 1 is $c \n";
+$typec = gettype($c);
+echo "Type: $typec \n"; */
+
+  
+  
+/* $d = '1' + (double)'1';
+echo "'1' + '1' is $d \n";
+$typed = gettype($d);
+echo "Type: $typed \n"; 
+  */
+ 
+ 
+  /*
+  $array = ["Allison" => "Buchholtz-Au", "Ema" =>"Horvath"];
+  echo "{$array['Ema']}\n";
+  */
+  
+/*   
+  $array2 = [3, 5, 6];
+  echo "{$array2[0]}\n";
+ 
+   */
+  /*
+  $arr = array("", "two", "three");
+  foreach ($arr as $key => $counter) {
+    echo "Value:$key => $counter\n";
+  }
+  */
+  
+/* 	$arr = array("a" => 1, "b" => "two", "c" => "three");
+	foreach ($arr as $key => $value) {
+	echo "Key: $key; Value: $value\n";
+	} */
+  
+?>
